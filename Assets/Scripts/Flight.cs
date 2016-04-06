e07d25834e232edc805a2c523fa9897c68ea5c98
PlanePilotProject <planepilotproject@inbox.lt>
2016-04-06 22:19:11 +0300

    Unity
    Unity project

---
+++ Assets/Scripts/Flight.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flight : MonoBehaviour {
+
+    public float flightSpeed = 50.0f;
+    public float rotateSpeed = 25.0f;
+    float fixForward = 0.0f;
+    float fixRotate = 0.0f;
+    void Update()
+    {
+        fixForward = flightSpeed * Time.deltaTime;
+        fixRotate = rotateSpeed * Time.deltaTime;
+        FlightControlls();
+    }
+
+	void FlightControlls()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Debug.Log("THdown right");
+                transform.Translate(0, 0, fixForward * (Input.GetAxis("Vertical") + Input.GetAxis("Vertical")));
+                transform.Rotate(0, fixRotate * Input.GetAxis("Horizontal"), 0);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                Debug.Log("THdown left");
+                transform.Translate(0, 0, fixForward * (Input.GetAxis("Vertical") + Input.GetAxis("Vertical")));
+                transform.Rotate(0, fixRotate * Input.GetAxis("Horizontal"), 0);
+            }
+            else
+            {
+                Debug.Log("THdown");
+                transform.Translate(0, 0, fixForward * (Input.GetAxis("Vertical") + Input.GetAxis("Vertical")));
+            }
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Debug.Log("THup right");
+                transform.Translate(0, 0, fixForward);
+                transform.Rotate(0, fixRotate * Input.GetAxis("Horizontal"), 0);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                Debug.Log("THup left");
+                transform.Translate(0, 0, fixForward);
+                transform.Rotate(0, fixRotate * Input.GetAxis("Horizontal"), 0);
+            }
+            else
+            {
+                Debug.Log("THup");
+                transform.Translate(0, 0, fixForward);
+            }
+        }
+        else
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Debug.Log("THright");
+                transform.Translate(0, 0, fixForward);
+                transform.Rotate(0, fixRotate * Input.GetAxis("Horizontal"), 0);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                Debug.Log("THleft");
+                transform.Translate(0, 0, fixForward);
+                transform.Rotate(0, fixRotate * Input.GetAxis("Horizontal"), 0);
+            }
+            else
+            {
+                transform.Translate(0, 0, fixForward);
+                Debug.Log("THdir idle");
+            }
+        }
+        Debug.Log(Input.GetAxis("RVertical") + "::::" + Input.GetAxis("RHorizontal"));
+        if (Input.GetAxis("RVertical") > 0)
+        {
+            if (Input.GetAxis("RHorizontal") > 0)
+            {
+                Debug.Log("down right");
+                transform.Rotate(fixRotate * Input.GetAxis("RVertical"), 0, fixRotate * Input.GetAxis("RHorizontal"));
+            }
+            else if (Input.GetAxis("RHorizontal") < 0)
+            {
+                Debug.Log("down left");
+                transform.Rotate(fixRotate * Input.GetAxis("RVertical"), 0, fixRotate * Input.GetAxis("RHorizontal"));
+            }
+            else
+            {
+                Debug.Log("down");
+                transform.Rotate(fixRotate * Input.GetAxis("RVertical"), 0, 0);
+            }
+        }
+        else if (Input.GetAxis("RVertical") < 0)
+        {
+            if (Input.GetAxis("RHorizontal") > 0)
+            {
+                Debug.Log("up right");
+                transform.Rotate(fixRotate * Input.GetAxis("RVertical"), 0, fixRotate * Input.GetAxis("RHorizontal"));
+            }
+            else if (Input.GetAxis("RHorizontal") < 0)
+            {
+                Debug.Log("up left");
+                transform.Rotate(fixRotate * Input.GetAxis("RVertical"), 0, fixRotate * Input.GetAxis("RHorizontal"));
+            }
+            else
+            {
+                Debug.Log("up");
+                transform.Rotate(fixRotate * Input.GetAxis("RVertical"), 0, 0);
+            }
+        }
+        else
+        {
+            if (Input.GetAxis("RHorizontal") > 0)
+            {
+                Debug.Log("right");
+                transform.Rotate(0, 0, fixRotate * Input.GetAxis("RHorizontal"));
+            }
+            else if (Input.GetAxis("RHorizontal") < 0)
+            {
+                Debug.Log("left");
+                transform.Rotate(0, 0, fixRotate * Input.GetAxis("RHorizontal"));
+            }
+            else
+            {
+                Debug.Log("dir idle");
+                transform.Rotate(0, 0, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movements.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movements : MonoBehaviour {
+
+    enum Movement
+    {
+        moveForward,
+        moveBackward,
+        moveLeft,
+        moveRight,
+        moveForwardLeft,
+        moveForwardRight,
+        moveBackwardLeft,
+        moveBackwardRight,
+        idle
+    };
+    private Movement mstate;
+
+    public float forwardSpeed = 10.0f;
+    public float backwardSpeed = 5.0f;
+    public float strafeSpeed = 7.0f;
+	// Update is called once per frame
+	void Update () {
+        DetermineDirection();
+        Move();
+	}
+
+    void Move()
+    {
+        switch (mstate)
+        {
+            case Movement.moveForward:
+                transform.position += transform.forward * forwardSpeed * Time.deltaTime;
+                break;
+            case Movement.moveForwardLeft:
+                transform.position += transform.forward * forwardSpeed * Time.deltaTime;
+                transform.position -= transform.right;
+                break;
+            case Movement.moveForwardRight:
+                transform.position += transform.forward * forwardSpeed * Time.deltaTime;
+                transform.position += transform.right;
+                break;
+            case Movement.moveBackward:
+                transform.position -= transform.forward * backwardSpeed * Time.deltaTime;
+                break;
+            case Movement.moveBackwardLeft:
+                transform.position -= transform.forward * backwardSpeed * Time.deltaTime;
+                transform.position -= transform.right;
+                break;
+            case Movement.moveBackwardRight:
+                transform.position -= transform.forward * backwardSpeed * Time.deltaTime;
+                transform.position += transform.right;
+                break;
+            case Movement.moveRight:
+                transform.position += transform.right * strafeSpeed * Time.deltaTime;
+                break;
+            case Movement.moveLeft:
+                transform.position -= transform.right * strafeSpeed * Time.deltaTime;
+                break;
+            case Movement.idle:
+
+                break;
+        }
+        Debug.Log(mstate);
+    }
+
+    void DetermineDirection()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                mstate = Movement.moveForwardRight;
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                mstate = Movement.moveForwardLeft;
+            }
+            else
+            {
+                mstate = Movement.moveForward;
+            }
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                mstate = Movement.moveBackwardRight;
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                mstate = Movement.moveBackwardLeft;
+            }
+            else
+            {
+                mstate = Movement.moveBackward;
+            }
+        }
+        else
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                mstate = Movement.moveRight;
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                mstate = Movement.moveLeft;
+            }
+            else
+            {
+                mstate = Movement.idle;
+            }
+        }
+    }
+}
